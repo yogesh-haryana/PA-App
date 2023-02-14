74815d6f15ad1cebd6a4c2e0e4668c37f8be5051
yogesh-haryana <personalyogesh352@gmail.com>
2023-02-14 17:53:52 +0530

    side menu changed according to setting toggle

--- src/components/DisplaySection.js
+++ src/components/DisplaySection.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 import displaySectionStyles from "../Styles/displaySectionStyles";
 import InfoContainer from "./InfoContainer";
 import SideBarComp from "./SideBarComp";
 
 function DisplaySection(props) {
   const classes = displaySectionStyles();
+  const [isFormAvaliable, setFormAvailable] = useState();
   const menuItemsForManagement = ["Employee List", "KRA List", "Goal List", "Performance Report", "Settings"];
-  const menuItemsForEngineers = ["Performance Report", "Appraisal Form"];
-  const menuItemsForManager = ["Performance Report", "Appraisal Rating"];
+  const menuItemsForEngineersOn = ["Performance Report", "Appraisal Form"];
+  const menuItemsForManagerOn = ["Performance Report", "Appraisal Rating"];
+  const menuItemsForEngineersOff = ["Performance Report"];
+  const menuItemsForManagerOff = ["Performance Report"];
+
+  const getSetting = async () => {
+    const resp = await axios.get("http://localhost:8080/api/settings");
+    const { data } = resp;
+    if (data.length === 1) {
+      setFormAvailable(data[0].toggle);
+    }
+  };
+
+  useEffect(() => {
+    getSetting();
+  }, []);
 
   const { user } = props;
   const { role } = user;
@@ -15,8 +32,8 @@ function DisplaySection(props) {
   return (
     <div className={classes.mainContainer}>
       {role === "Management" && <SideBarComp menuItemsForSideBar={menuItemsForManagement} user={user} />}
-      {role === "Manager" && <SideBarComp menuItemsForSideBar={menuItemsForManager} user={user} />}
-      {role === "Engineer" && <SideBarComp menuItemsForSideBar={menuItemsForEngineers} user={user} />}
+      {role === "Manager" && <SideBarComp menuItemsForSideBar={isFormAvaliable ? menuItemsForManagerOn : menuItemsForManagerOff} user={user} />}
+      {role === "Engineer" && <SideBarComp menuItemsForSideBar={isFormAvaliable ? menuItemsForEngineersOn : menuItemsForEngineersOff} user={user} />}
       <InfoContainer />
     </div>
   );
